1c7984678cf5ed7c142f33dc860f156eb4dc8670
hiren1204 <47883219+hiren1204@users.noreply.github.com>
2020-08-24 09:43:19 +0530

    dcc.Tab component added

--- Project/dashboard_1.py
+++ Project/dashboard_1.py
@@ -12,7 +12,7 @@ date_wise_total_csv = pd.read_csv("E:\Study\sem7\BDAD\Dashboard\data\date_wise_t
 latest_stat = pd.read_csv("E:\Study\sem7\BDAD\Dashboard\data\latest_stats.csv")
 
 stat_date_wise = pd.read_csv("E:\Study\sem7\BDAD\Dashboard\data\state_date_wise2.csv")
-#stock = pd.read_csv("E:\Study\sem7\BDAD\stockMarket\HUL_Stock.csv")
+# stock = pd.read_csv("E:\Study\sem7\BDAD\stockMarket\HUL_Stock.csv")
 #### ----- Step 2 (Plot data)----
 
 # Plot column 'Confirmed'
@@ -44,13 +44,13 @@ def sum_of_discharged():
     return total_discharged
 
 
-#stylesheet = 'https://codepen.io/chriddyp/pen/bWLwgP.css'
-#app = dash.Dash(__name__, external_stylesheets=stylesheet)
+# stylesheet = 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+# app = dash.Dash(__name__, external_stylesheets=stylesheet)
 app = dash.Dash(__name__)
 app.layout = html.Div([
     html.Link(
         rel='stylesheet',
-        href='E:\Study\sem7\BDAD\Project\assets\test.css',
+        href='./../assets/test.css',
         # href='E:\Study\sem7\BDAD\Project\assets\css.css'
     ),
 
@@ -69,175 +69,33 @@ app.layout = html.Div([
                  ])
                  ),
 
-    ]),
+    ], className='main_header'),
 
     # body
+
     html.Div([
-        # for covid
-        html.Div([
-            html.Div(
-                children=html.Div([
-                    html.H2("COVID-19 Data"),
-                ])
-            ),
-            html.Div([html.Div([
-                html.P("Confirmed", className='text'),
-                html.P(children=sum_of_confirmed_cases(),
-                       id='text')],
-                className="mini_container",
-            ),
-                html.Div(
-                    [html.P("Active", className='text'),
-                     html.P(children=sum_of_active_cases(),
-                            id='ActiveCases')],
-                    # id="totalConfirmed",
-                    className="mini_container",
-                ),
-                html.Div(
-                    [html.P("Discharged", className='text'),
-                     html.P(children=sum_of_discharged(),
-                            id='Discharged')],
-                    # id="totalConfirmed",
-                    className="mini_container",
-                ),
-                html.Div(
-                    [html.P("Deaths", className='text'),
-                     html.P(children=sum_of_deaths(),
-                            id='Deaths')],
-                    # id="totalConfirmed",
-                    className="mini_container",
-                )],
-                id="info-container",
-                className="row container-display",
-            ),
-
-            html.Div(
-                children=html.Div([
-                    dash_table.DataTable(
-                        id='datatable_id',
-                        data=latest_stat.to_dict('records'),
-                        columns=[{"name": i, "id": i, "deletable": False, "selectable": False} for i in
-                                 latest_stat.columns
-                                 ],
-                        editable=False,
-                        sort_action="native",
-                        sort_mode="multi",
-                        row_selectable="multi",
-                        row_deletable=False,
-                        selected_rows=[],
-                        # page_action="native",
-                        # page_current= 0,
-                        # page_size= 6,
-                        # page_action='none',
-                        style_table={
-                            'maxHeight': '40ex',
-                            'overflowY': 'scroll',
-                            'width': '20ex',
-                            'minWidth': '100%',
-                        },
-                        fixed_rows={'headers': True, 'data': 0},
-                        virtualization=True,
-                        # style_cell_conditional=[
-                        #    {'if': {'column_id': 'Location'},
-                        #     'width': '10%', 'textAlign': 'left'},
-                        #    {'if': {'column_id': 'Deaths'},
-                        #     'width': '30%', 'textAlign': 'left'},
-                        #   {'if': {'column_id': 'cases'},
-                        #     'width': '30%', 'textAlign': 'left'},
-                        #   ],
-                    )
-                ])
-            ),
-            html.Div(className="pie_and_line_chart",
-                     children=html.Div([
-                         dcc.Dropdown(id='linedropdown',
-                                      options=[
-                                          {'label': 'Deaths', 'value': 'Deaths'},
-                                          {'label': 'TotalConfirmed', 'value': 'TotalConfirmed'}
-                                      ],
-                                      value='Deaths',
-                                      multi=False,
-                                      clearable=False
-                                      ),
-                         html.Div([
-                             dcc.Graph(id='piechart'),
-                         ]),
-                     ]),
-                     ),
-            html.Div(className="pie_and_line_chart",
-                     children=html.Div([
-                         dcc.Dropdown(id='piedropdown',
-                                      options=[{'label': 'Deaths', 'value': 'Deaths'},
-                                               {'label': 'TotalConfirmed', 'value': 'TotalConfirmed'}
-                                               ],
-                                      value='TotalConfirmed',
-                                      multi=False,
-                                      clearable=False
-                                      ),
-                         html.Div([
-                             dcc.Graph(id='linechart'),
-                         ]),
-                     ]),
-                     ),
-        ], className='six column'),
-        #
-        # # for stock market
-        # html.Div([
-        #     #        html.Div(className = "column",
-        #     #                children=html.Div([
-        #     #                    html.H2("Stock Market Data"),
-        #     #                    ])
-        #     #                ),
-        #     #       html.Div(className="latest_stat",
-        #     #                  children=html.Div([
-        #     dash_table.DataTable(
-        #         id='datatable_id2',
-        #         data=stat_date_wise.to_dict('records'),
-        #         columns=[
-        #             {"name": i, "id": i, "deletable": False, "selectable": False} for i in stat_date_wise.columns
-        #         ],
-        #         editable=False,
-        #         sort_action="native",
-        #         sort_mode="multi",
-        #         row_selectable="multi",
-        #         row_deletable=False,
-        #         selected_rows=[],
-        #         # page_action="native",
-        #         # page_current= 0,
-        #         # page_size= 6,
-        #         # page_action='none',
-        #         style_table={
-        #             'maxHeight': '35ex',
-        #             'overflowY': 'scroll',
-        #             'width': '100%',
-        #             'minWidth': '100%',
-        #         },
-        #         fixed_rows={'headers': True, 'data': 0},
-        #         virtualization=True,
-        #         # style_cell_conditional=[
-        #         #     {'if': {'column_id': 'Location'},
-        #         #     'width': '10%', 'textAlign': 'left'},
-        #         #    {'if': {'column_id': 'Deaths'},
-        #         #     'width': '30%', 'textAlign': 'left'},
-        #         #   {'if': {'column_id': 'cases'},
-        #         #     'width': '30%', 'textAlign': 'left'},
-        #         #   ],
-        #     )
-        # ])
-
-    ], ),
+        dcc.Tabs(id='tabs', value='covid' , children=[
+            dcc.Tab(label='covid', value='covid'),
+            dcc.Tab(label='Stock Market', value='stock market'),
+            dcc.Tab(label='Insights', value='insights'),
+        ]),
+        html.Div(id='tabs-content')
+    ])
 ])
+
 
 @app.callback(
     [Output('linechart', 'figure'),
-     Output('piechart', 'figure')],
+     Output('piechart', 'figure'),
+     Output('barchart', 'figure'),],
     [Input('datatable_id', 'selected_rows'),
      Input('piedropdown', 'value'),
-     Input('linedropdown', 'value')]
+     Input('linedropdown', 'value'),
+     Input('bardropdown', 'value')]
 )
-def update_data(chosen_rows, piedropval, linedropval):
+def update_data(chosen_rows, piedropval, linedropval,bardropval):
     if len(chosen_rows) == 0:
-        df_filterd = latest_stat[latest_stat['Location'].isin(['Kerala', 'Gujarat', 'Punjab'])]
+        df_filterd = latest_stat[latest_stat['Location'].isin(['Kerala', 'Gujarat', 'Punjab' , 'Manipur' , 'Arunachal Pradesh', 'Chandigarh'])]
     else:
         print(chosen_rows)
         df_filterd = latest_stat[latest_stat.index.isin(chosen_rows)]
@@ -250,6 +108,13 @@ def update_data(chosen_rows, piedropval, linedropval):
         labels={'Location': 'States'}
     )
 
+    bar_chart = px.bar(
+        data_frame=df_filterd,
+        x='Location',
+        y=bardropval,
+        color='Location',
+        labels={'Location': 'States'}
+    )
     # extract list of chosen countries
     list_chosen_state = df_filterd['Location'].tolist()
     # filter original df according to chosen countries
@@ -265,7 +130,203 @@ def update_data(chosen_rows, piedropval, linedropval):
     )
     # line_chart.update_layout(uirevision='foo')
 
-    return (pie_chart, line_chart)
+    return (pie_chart, line_chart , bar_chart)
+
+
+#
+# @app.callback(
+#     Output('cases', 'children'),
+#     [Input(component_id='stat_dropdown', component_property='value')]
+# )
+#
+# def print_stat_data(stat_dropdown_val):
+#     if stat_dropdown_val  = 'Delhi':
+#         select_stat = latest_stat['Location']['']
+#     # return sum(total_conf_cases)
+#     return 'You have selected "{}"'.format(stat_dropdown_val)
+
+
+@app.callback(Output('tabs-content', 'children'),
+              [Input('tabs', 'value')])
+def render_content(tab):
+    if tab == 'covid':
+        return html.Div([
+
+            # for covid
+            html.Div([
+                html.Div(
+                    children=html.Div([
+                        html.H2("COVID-19 Data"),
+                    ])
+                ),
+                html.Div([html.Div([
+                    html.P("Confirmed", className='text'),
+                    html.P(children=sum_of_confirmed_cases(),
+                           id='text')],
+                    className="mini_container",
+                ),
+                    html.Div(
+                        [html.P("Active", className='text'),
+                         html.P(children=sum_of_active_cases(),
+                                id='ActiveCases')],
+                        # id="totalConfirmed",
+                        className="mini_container",
+                    ),
+                    html.Div(
+                        [html.P("Discharged", className='text'),
+                         html.P(children=sum_of_discharged(),
+                                id='Discharged')],
+                        # id="totalConfirmed",
+                        className="mini_container",
+                    ),
+                    html.Div(
+                        [html.P("Deaths", className='text'),
+                         html.P(children=sum_of_deaths(),
+                                id='Deaths')],
+                        # id="totalConfirmed",
+                        className="mini_container",
+                    )],
+                    id="info-container",
+                    className="row container-display",
+                ),
+
+                html.Div(
+                    children=html.Div([
+                        dash_table.DataTable(
+                            id='datatable_id',
+                            data=latest_stat.to_dict('records'),
+                            columns=[{"name": i, "id": i, "deletable": False, "selectable": False} for i in
+                                     latest_stat.columns
+                                     ],
+                            editable=False,
+                            sort_action="native",
+                            sort_mode="multi",
+                            row_selectable="multi",
+                            row_deletable=False,
+                            selected_rows=[],
+                            # page_action="native",
+                            # page_current= 0,
+                            # page_size= 6,
+                            # page_action='none',
+                            style_table={
+                                'maxHeight': '40ex',
+                                'overflowY': 'scroll',
+                                'width': '20ex',
+                                'minWidth': '100%',
+                            },
+                            fixed_rows={'headers': True, 'data': 0},
+                            virtualization=True,
+                            style_cell_conditional=[
+                                {'if': {'column_id': 'Location'},
+                                 'width': '10%'},
+
+                                #    {'if': {'column_id': 'Deaths'},
+                                #     'width': '30%', 'textAlign': 'left'},
+                                #   {'if': {'column_id': 'cases'},
+                                #     'width': '30%', 'textAlign': 'left'},
+                            ],
+                        )
+                    ])
+                ),
+                html.Div(className="",
+                         children=html.Div([
+                             dcc.Dropdown(id='linedropdown',
+                                          options=[
+                                              {'label': 'Deaths', 'value': 'Deaths'},
+                                              {'label': 'TotalConfirmed', 'value': 'TotalConfirmed'}
+                                          ],
+                                          value='TotalConfirmed',
+                                          multi=False,
+                                          clearable=False
+                                          ),
+                             html.Div([
+                                 dcc.Graph(id='piechart'),
+                             ]),
+                         ]),
+                         ),
+                html.Div(className="pie_and_line_chart",
+                         children=html.Div([
+                             dcc.Dropdown(id='piedropdown',
+                                          options=[{'label': 'Deaths', 'value': 'Deaths'},
+                                                   {'label': 'TotalConfirmed', 'value': 'TotalConfirmed'}
+                                                   ],
+                                          value='TotalConfirmed',
+                                          multi=False,
+                                          clearable=False
+                                          ),
+                             html.Div([
+                                 dcc.Graph(id='linechart'),
+                             ]),
+                         ]),
+                         ),
+                html.Div(className="pie_and_line_chart",
+                         children=html.Div([
+                             dcc.Dropdown(id='bardropdown',
+                                          options=[{'label': 'Deaths', 'value': 'Deaths'},
+                                                   {'label': 'TotalConfirmed', 'value': 'TotalConfirmed'}
+                                                   ],
+                                          value='TotalConfirmed',
+                                          multi=False,
+                                          clearable=False
+                                          ),
+                             html.Div([
+                                 dcc.Graph(id='barchart'),
+                             ]),
+                         ]),
+                         ),
+            ]),
+        ])
+    elif tab == 'stock market':
+        return html.Div([
+            # # for stock market
+            html.Div([
+                html.Div(className="column",
+                         children=html.Div([
+                             html.H2("Stock Market Data"),
+                         ])
+                         ),
+                #     #       html.Div(className="latest_stat",
+                #     #                  children=html.Div([
+                #     dash_table.DataTable(
+                #         id='datatable_id2',
+                #         data=stat_date_wise.to_dict('records'),
+                #         columns=[
+                #             {"name": i, "id": i, "deletable": False, "selectable": False} for i in stat_date_wise.columns
+                #         ],
+                #         editable=False,
+                #         sort_action="native",
+                #         sort_mode="multi",
+                #         row_selectable="multi",
+                #         row_deletable=False,
+                #         selected_rows=[],
+                #         # page_action="native",
+                #         # page_current= 0,
+                #         # page_size= 6,
+                #         # page_action='none',
+                #         style_table={
+                #             'maxHeight': '35ex',
+                #             'overflowY': 'scroll',
+                #             'width': '100%',
+                #             'minWidth': '100%',
+                #         },
+                #         fixed_rows={'headers': True, 'data': 0},
+                #         virtualization=True,
+                #         # style_cell_conditional=[
+                #         #     {'if': {'column_id': 'Location'},
+                #         #     'width': '10%', 'textAlign': 'left'},
+                #         #    {'if': {'column_id': 'Deaths'},
+                #         #     'width': '30%', 'textAlign': 'left'},
+                #         #   {'if': {'column_id': 'cases'},
+                #         #     'width': '30%', 'textAlign': 'left'},
+                #         #   ],
+                #     )
+            ])
+
+        ])
+    elif tab == 'insights':
+        return html.Div([
+            html.H3('Insights')
+        ])
 
 
 if __name__ == "__main__":
